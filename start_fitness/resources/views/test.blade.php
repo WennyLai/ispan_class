4dc6eafe6ca904e61c15ed5c0c873295aff27f82
WennyLai <jom30801@gmail.com>
2022-07-19 15:22:07 +0800

    0719 新增套件 magic-master

---
+++ start_fitness/resources/views/test.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/magic-master/dist/magic.css" rel="stylesheet">
+
+
+    <title>Document</title>
+    <style>
+        div {
+            position: absolute;
+            display: block;
+            width: 10vw;
+            height: 10vh;
+            background-color: aqua;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="hi">123</div>
+    <script>
+        function myFunction() {
+            const selector = document.querySelector('.hi')
+            // selector.classList.add('magictime', 'slideRight')
+            selector.classList.toggle('slideRight')
+            selector.classList.toggle('magictime')
+        }
+        setInterval(myFunction, 1000);
+    </script>
+</body>
+
+</html>
