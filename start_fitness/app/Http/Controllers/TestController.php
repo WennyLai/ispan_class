01024672a20403a09c511947df1f427a3ae5f632
WennyLai <jom30801@gmail.com>
2022-07-21 11:15:33 +0800

    0721 test trait

--- start_fitness/app/Http/Controllers/TestController.php
+++ start_fitness/app/Http/Controllers/TestController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits\TraitTest;
+
 
 class TestController extends Controller
-{
+{   
+    use TraitTest;
     function index() {
-        return view('ld.member.test');
+        
+        return $this->hello();
     }
 }
---
+++ start_fitness/app/Traits/TraitTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Traits;
+
+use Exception;
+use Illuminate\Http\Request;
+
+    trait TraitTest {
+
+     function hello() {
+        return 'hello 2222222222222';
+     }
+    }
+
+
+
+
+
+?>
